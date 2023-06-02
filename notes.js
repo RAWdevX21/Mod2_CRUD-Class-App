20a7f223535b2ede51fc8cd401961682a7468f88
RicDW <rawdevx21@gmail.com>
2023-06-02 11:29:47 -0400

    update package.json, added data/, animals.json, src/ & modified notes.js

---
+++ notes.js
@@ -0,0 +1,8 @@
+const { readJSONFile, writeJSONFile } = require("./src/helpers")
+console.log("This is the Notes.js file");
+
+function run() {
+    console.log("Welcome to Our Animals App! 🦁")
+}
+
+run()
---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+const { readFileSync, writeFileSync } = require("node:fs")
+
+function readJSONFile(path, fileName) {
+  // readFileSync
+  const object = readFileSync(`${path}/${fileName}`, "utf8");
+  return object ? JSON.parse(object) : [];
+}
+
+function writeJSONFile(path, fileName, data) {
+  // writeFileSync
+  data = JSON.stringify(data);
+  return writeFileSync(`${path}/${fileName}`, data, { encoding: "utf-8" });
+}
+
+module.exports {
+    readJSONFile,
+    writeJSONFile
+}
